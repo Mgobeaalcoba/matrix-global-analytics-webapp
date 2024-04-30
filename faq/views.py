157089af231849d9bbad1518290928bdd960ba13
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-04-30 18:45:25 -0300

    Render Django FAQ

--- faq/views.py
+++ faq/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import FAQ
 
 # Create your views here.
 def faq_view(request):
-    return render(request, 'faq/preguntas_frecuentes.html')
+    faq_items = FAQ.objects.all()
+    context = {
+        'faq_items': faq_items
+    }
+    return render(request, 'faq/preguntas_frecuentes.html', context)
