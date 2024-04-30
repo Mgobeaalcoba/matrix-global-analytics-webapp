1bae75b9dd09826a9c5051a40c5e4cb988608549
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-04-30 20:30:43 -0300

    Render Django 'precios' section

--- precios/models.py
+++ precios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Paquete(models.Model):
+    titulo = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
+    caracteristicas = models.TextField()
+
+    def __str__(self):
+        return self.titulo
--- precios/views.py
+++ precios/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Paquete
 
 # Create your views here.
 def precios(request):
-    return render(request, 'precios/precios.html')
+    paquetes = Paquete.objects.all()
+    for paquete in paquetes:
+        int_precio = int(paquete.precio)
+        paquete.precio = int_precio
+        caracter_list = paquete.caracteristicas.split("|")
+        paquete.caracteristicas = caracter_list
+    context = {
+        'paquetes': paquetes
+    }
+    return render(request, 'precios/precios.html', context)
