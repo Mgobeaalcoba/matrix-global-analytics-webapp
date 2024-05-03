aadcc8bedf912da3e438dc0d56e9f37488ef98d4
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-03 12:01:39 -0300

    :white_check_mark: 'Cursos' App with coverage 100%

--- cursos/tests.py
+++ cursos/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from django.template.loader import render_to_string
 from django.urls import reverse
 from .models import Curso
 
@@ -28,6 +29,7 @@ class CursosTestCase(TestCase):
         self.assertEqual(curso.descripcion, "Descripción del Curso 1")
         self.assertEqual(curso.precio, 1000)
         self.assertEqual(curso.imagen, "../../../static/i/curso1.jpg")
+        self.assertEqual(curso.__str__(), 'Curso 1')
 
 
 class CursosViewsTestCase(TestCase):
@@ -64,3 +66,41 @@ class CursosViewsTestCase(TestCase):
         self.assertEqual(curso.precio, 1000)
         self.assertEqual(curso.imagen, "../../../static/i/test_logo.jpg")
 
+class TestCursosTemplate(TestCase):
+    def setUp(self):
+        # Crea algunos cursos de ejemplo
+        self.curso1 = Curso.objects.create(
+            titulo="Curso 1",
+            descripcion="Descripción del curso 1 | Detalle 1 | Detalle 2",
+            precio=100,
+            imagen="imagen1.jpg"
+        )
+        self.curso2 = Curso.objects.create(
+            titulo="Curso 2",
+            descripcion="Descripción del curso 2 | Detalle 1 | Detalle 2",
+            precio=200,
+            imagen="imagen2.jpg"
+        )
+        courses_list = [self.curso1, self.curso2]
+        for course in courses_list:
+            course.descripcion = course.descripcion.split("|")
+            course.save()
+
+    def test_cursos_template(self):
+        # Renderiza el template con los cursos
+        rendered_template = render_to_string('cursos/cursos.html', context={'cursos': [self.curso1, self.curso2]})
+
+        # Verifica que el contenido esperado esté presente en el template renderizado
+        self.assertInHTML('<h3>Curso 1</h3>', rendered_template)
+        self.assertInHTML('<h3>Curso 2</h3>', rendered_template)
+        self.assertInHTML('<li>Descripción del curso 1 </li>', rendered_template)
+        self.assertInHTML('<li>Detalle 1 </li>', rendered_template)
+        self.assertInHTML('<li>Detalle 2 </li>', rendered_template)
+        self.assertInHTML('<li>Descripción del curso 2 </li>', rendered_template)
+        self.assertInHTML('<li>Detalle 1 </li>', rendered_template)
+        self.assertInHTML('<li>Detalle 2 </li>', rendered_template)
+        self.assertInHTML('<img src="imagen1.jpg" alt="Logo del Curso 1" class="course-image">', rendered_template)
+        self.assertInHTML('<img src="imagen2.jpg" alt="Logo del Curso 2" class="course-image">', rendered_template)
+        self.assertInHTML('<p class="course-price">USD 100</p>', rendered_template)
+        self.assertInHTML('<p class="course-price">USD 200</p>', rendered_template)
+        self.assertInHTML('<button class="course-btn" onclick="window.location.href=\'/contacto/\'">Contratar</button>', rendered_template)
--- faq/tests.py
+++ faq/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from .models import FAQ
 
 class FAQModelTest(TestCase):
@@ -14,3 +15,32 @@ class FAQModelTest(TestCase):
         self.assertEqual(faq.question, "Test Question")
         self.assertEqual(faq.answer, "Test Answer")
 
+class FAQViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_faq_view(self):
+        # Crear FAQ de prueba
+        FAQ.objects.create(
+            question="¿Pregunta de prueba?",
+            answer="Respuesta de prueba"
+        )
+
+        # Obtener la URL para la vista FAQ
+        url = reverse('preguntas_frecuentes')
+
+        # Realizar una solicitud GET a la vista FAQ
+        response = self.client.get(url)
+
+        # Verificar que la respuesta sea exitosa
+        self.assertEqual(response.status_code, 200)
+
+        # Verificar que el template correcto esté siendo utilizado
+        self.assertTemplateUsed(response, 'faq/preguntas_frecuentes.html')
+
+        # Verificar que el FAQ creado esté presente en el contexto de la vista
+        faq_items = response.context['faq_items']
+        self.assertEqual(len(faq_items), 1)
+        faq_item = faq_items[0]
+        self.assertEqual(faq_item.question, "¿Pregunta de prueba?")
+        self.assertEqual(faq_item.answer, "Respuesta de prueba")
